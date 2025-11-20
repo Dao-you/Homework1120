faecee8e1d1c31a51421435ea3abec43fb57debd
Dao_you <qaz00639@gmail.com>
2025-11-20 23:12:31 +0800

    complete code and some readme

---
+++ Lab5/app/src/main/java/com/example/lab5/ViewPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.lab5
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(
+    fm: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fm, lifecycle) {
+
+    override fun getItemCount(): Int = 4
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            2 -> testFragment()
+            else -> ThirdFragment()
+        }
+    }
+}
